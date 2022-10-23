899e9c7a386eec3ceafda7ba986703db3a67950f
alex <alexxxkny@mail.ru>
2022-10-23 13:53:31 +0300

    init commit, lab1: pretty teapot

---
+++ lab1/src/main.rs
@@ -0,0 +1,84 @@
+use glium::glutin;
+use glutin::event::*;
+use glium::{uniform, Surface};
+
+mod teapot;
+
+fn main() {
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(
+        &display,
+        glium::index::PrimitiveType::TrianglesList,
+        &teapot::INDICES
+    ).unwrap();
+
+    let vertex_shader = r#"
+        #version 150
+
+        in vec3 position;
+        in vec3 normal;
+
+        out vec3 v_normal;
+
+        uniform mat4 matrix;
+
+        void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = matrix * vec4(position, 1.0);
+        }
+    "#;
+
+    let pixel_shader = r#"
+        #version 150
+
+        in vec3 v_normal;
+        out vec4 color;
+        uniform vec3 u_light;
+
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.0, 0.6, 0.8);
+            vec3 regular_color = vec3(0.2, 0.9, 0.8);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader, pixel_shader, None).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_millis(16);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match event {
+            Event::WindowEvent {event, ..} => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return
+            },
+            _ => ()
+        }
+
+        let uniform = uniform! {
+            matrix: [
+                [0.01, 0.0, 0.0, 0.0],
+                [0.0, 0.01, 0.0, 0.0],
+                [0.0, 0.0, 0.01, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32]
+            ],
+            u_light: [-1.0, 0.4, 0.9f32]
+        };
+
+        let mut target = display.draw();
+        target.clear_color(1.0, 1.0, 1.0, 1.0);
+        target.draw((&positions, &normals), &indices, &program, &uniform, &Default::default()).unwrap();
+        target.finish().unwrap();
+    });
+}
